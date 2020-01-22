dacdcb1f19a14a2899360cf3d9c8777046bda3bd
Irakoze Franco-davy <irakoze@b10p5.fil.univ-lille1.fr>
2020-01-22 14:19:20 +0100

    torique done

--- Agent.py
+++ Agent.py
@@ -18,9 +18,29 @@ class Agent:
         [-1][0] |  [A]    |[1][0]
         [-1][-1]| [0][-1]|[1][-1]
         """
-        possible = [(self.posX-1, self.posY+1),(self.posX, self.posY+1),(self.posX+1, self.posY+1),(self.posX-1, self.posY),(self.posX+1, self.posY),(self.posX-1, self.posY-1),(self.posX, self.posY-1),(self.posX+1, self.posY-1)]
-        voisins = {'deplacement': [], 'agent' : []}
         
+        possible = []
+        voisins = {'deplacement': [], 'agent' : []}
+        if not self.environnement.instance.torique:
+            possible = [(self.posX-1, self.posY+1),(self.posX, self.posY+1),(self.posX+1, self.posY+1),(self.posX-1, self.posY),(self.posX+1, self.posY),(self.posX-1, self.posY-1),(self.posX, self.posY-1),(self.posX+1, self.posY-1)]
+        else:
+            
+            var = self.posX -1
+            var_1 = self.posX + 1
+            var_2 = self.posY - 1
+            var_3 = self.posY + 1
+            if self.posX-1 < 0:
+                var = taille - 1
+            if self.posX+1 > taille-1:
+                var_1 = 0
+            if self.posY -1 < 0:
+                var_2 = taille - 1
+            if self.posY + 1 > taille -1:
+                var_3 = 0
+            else:
+                pass
+            
+            possible = [(var, var_3),(self.posX, var_3),(var_1, var_3),(var, self.posY),(var_1, self.posY),(var, var_2),(self.posX, var_2),(var_1, var_2)]
         for i in possible :
             x,y = i
             if((x>=0 and x<taille) and (y>=0 and y<taille)):
