a4f6daf20b7c559d70dac286dca95bf43fad3fab
Vienne Jeremy <vienne@b10p27.fil.univ-lille1.fr>
2020-01-29 16:51:37 +0100

    time to live

--- Defender.py
+++ Defender.py
@@ -3,9 +3,9 @@ from Agent import Agent
 class Defender(Agent):
     def __init__(self,  posX, posY, env):
         
-        self.color = "brown"
+        self.color = "blue"
         self.time_to_live = 0
         super(Defender, self).__init__(posX, posY, env)
 
     def decide(self, taille) : 
-       return 0,0
+       self.time_to_live -= 1
